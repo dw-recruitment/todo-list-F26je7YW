e60a830752a0e1fa3973aafe33a02372ba4ac33f
Anonymous Candidate <anon@example.com>
2016-03-05 19:08:22 -0700

    adds ring + compojure, adds / route under construction message

--- project.clj
+++ project.clj
@@ -2,5 +2,14 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [ring "1.4.0"]
+                 [ring/ring-jetty-adapter "1.4.0"]
+                 [compojure "1.4.0"]]
+  :main todo.core
+  :profiles {:uberjar {:omit-source  true
+                       :aot          :all
+                       :uberjar-name "todo.jar"}
+             :dev     {:main         todo.core/-dev
+                       :repl-options {:init-ns user}}})
--- src/todo/core.clj
+++ src/todo/core.clj
@@ -1,6 +1,16 @@
-(ns todo.core)
+(ns todo.core
+  (:require [compojure.core :refer [GET defroutes]]
+            [compojure.route :as route]
+            [ring.adapter.jetty :as jetty]
+            [ring.middleware.reload :refer [wrap-reload]])
+  (:gen-class))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defroutes app
+           (GET "/" [] "<iframe src=\"//giphy.com/embed/l0t2DBLGpsd4A\" width=\"480\" height=\"597\" frameBorder=\"0\" class=\"giphy-embed\" allowFullScreen></iframe><p><a href=\"http://giphy.com/gifs/please-construction-patient-l0t2DBLGpsd4A\">via GIPHY</a></p>")
+           (route/not-found "<h1>Page not found</h1>"))
+
+(defn -main [port]
+  (jetty/run-jetty app {:port (Integer. port)}))
+
+(defn -dev [& [port]]
+  (jetty/run-jetty (wrap-reload #'app) {:port (Integer. (or port 3000))}))
