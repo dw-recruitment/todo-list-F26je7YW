d6a5a0cb717a9e96fb89e932abecae783238439b
Anonymous Candidate <anon@example.com>
2016-03-07 07:40:21 -0700

    adds form for submitting new todos, adds POST handler to create new todos in db

--- src/todo/core.clj
+++ src/todo/core.clj
@@ -1,10 +1,12 @@
 (ns todo.core
   (:require [todo.model :as model]
             [todo.views :as views])
-  (:require [compojure.core :refer [GET defroutes]]
+  (:require [compojure.core :refer [GET POST defroutes]]
             [compojure.route :as route]
             [ring.adapter.jetty :as jetty]
-            [ring.middleware.reload :refer [wrap-reload]])
+            [ring.middleware.reload :refer [wrap-reload]]
+            [ring.middleware.params :refer [wrap-params]]
+            [ring.util.response :refer [redirect]])
   (:gen-class))
 
 (def db "jdbc:postgresql://localhost/todos")
@@ -15,10 +17,20 @@
       (model/add-todo! db "foo" "desc 1")
       (model/add-todo! db "bar" "desc 2"))))
 
-(defroutes app
+(defn handle-post-new-todo [req]
+  (let [name (get-in req [:params "name"])
+        description (get-in req [:params "description"])
+        _ (model/add-todo! db name description)]
+    (redirect "/")))
+
+(defroutes app-routes
            (GET "/" [] (views/index (model/get-todos db)))
+           (POST "/add-new-todo" [] handle-post-new-todo)
            (GET "/about" [] "<h1>About</h1><h2>This project is about getting stuff done!</h2>")
            (route/not-found "<h1>Page not found</h1>"))
+(def app
+        (-> app-routes
+            wrap-params))
 
 (defn -main [port]
   (model/create-table! db)
--- src/todo/views.clj
+++ src/todo/views.clj
@@ -2,6 +2,15 @@
   (:require [hiccup.core :as h]
             [hiccup.page :as page]))
 
+(defn add-new-todo-form []
+  [:form {:action "/add-new-todo" :method "POST"}
+   [:p "add new:"]
+   [:p [:label [:b "title: "]
+        [:input {:type "text" :name "name"}]]]
+   [:p [:label [:b "description"]
+        [:input {:type "text" :name "description"}]]
+    [:input {:type "submit" :value "submit new todo"}]]])
+
 (defn index [content]
   (let [todos content]
     (page/html5
@@ -9,6 +18,7 @@
        [:title "Todos App"]]
       [:body
        [:h1 {:id "mount"} "TODOS:"]
+       (add-new-todo-form)
        [:ul
         (for [t todos]
           [:div.todo
